b43daf56f3080646e85c9456ac3610b587bdb944
Paulo Morpheus <paulormorpheus21@gmail.com>
2026-06-18 21:02:13 -0300

    fix(ui): correct closing tag for motion.div in DashboardLayout

--- src/pages/DashboardLayout.tsx
+++ src/pages/DashboardLayout.tsx
@@ -414,7 +414,7 @@ export default function DashboardLayout() {
       <BirthdayGlobalCelebration />
       <PWAInstallPrompt />
       <WelcomePackageCeremony />
-    </div>
+    </motion.div>
 
   );
 }
